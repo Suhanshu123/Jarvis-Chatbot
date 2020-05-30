cacb79077f738066befd052bc7373aa83ace9559
Suhanshu patel <patel1.suhanshu@gmail.com>
2020-05-30 15:53:09 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -108,7 +108,7 @@ elif 'email to me' in query.lower():
                             
     except Exception as e:
         print(e)
-        speak("Sorry my friend Suhanshu bhai. I am not able to send this email")
+        speak("Sorry . I am not able to send this email")
       
 
                 
